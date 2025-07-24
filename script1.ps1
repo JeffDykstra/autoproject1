869de9638386d82115e1297fe31366f412f758c6
JeffDykstra <JeffDykstra@users.noreply.github.com>
2025-07-24 09:12:12 -0500

    new function new function that does something useful

--- script1.ps1
+++ script1.ps1
@@ -1,2 +1,43 @@
 'testv5'
 'new line'
+function Get-DirectoryReport
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Path
+    )
+
+    if (-not (Test-Path $Path))
+    {
+        Write-Error "Path '$Path' does not exist."
+        return
+    }
+
+    $items = Get-ChildItem -Path $Path -Force
+    $report = @()
+
+    foreach ($item in $items)
+    {
+        $type = if ($item.PSIsContainer) { 'Directory' } else { 'File' }
+        $size = if ($item.PSIsContainer)
+        { 
+            (Get-ChildItem -Path $item.FullName -Recurse -File | Measure-Object -Property Length -Sum).Sum
+        }
+        else
+        {
+            $item.Length
+        }
+        $report += [PSCustomObject]@{
+            Name          = $item.Name
+            Type          = $type
+            Size          = $size
+            LastWriteTime = $item.LastWriteTime
+            FullPath      = $item.FullName
+        }
+    }
+
+    $report | Sort-Object Type, Name | Format-Table -AutoSize
+}
+
+# Example usage:
+# Get-DirectoryReport -Path "C:\Temp"
